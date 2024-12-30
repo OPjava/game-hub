1af2a9ea5c865954fcadd2e330008bdf67f4912e
John Micheal <kedeesjohn200@gmail.com>
2024-12-30 19:26:55 +0200

    Implment search functionality

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,14 @@ import logo from "../assets/profile-icon.jpeg";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="50px" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
